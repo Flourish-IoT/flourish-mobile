4c6b45fa1ccc3d6e7ca90efee2be07d95a33178b
Hunter Hampton <hmh84@drexel.edu>
2021-11-05 22:22:47 -0400

    Native styles, Photo Access, SVGs

--- screens/Home.js
+++ screens/Home.js
@@ -1,29 +1,57 @@
-import React, { useLayoutEffect, } from 'react';
-import { Platform, Linking, CameraRoll, StyleSheet } from 'react-native';
+import React, { useState, useLayoutEffect } from 'react';
+import { StyleSheet, Image, TouchableOpacity, Text, View } from 'react-native';
 import { Button } from 'react-native-elements';
+import * as ImagePicker from 'expo-image-picker';
+import { Camera } from 'expo-camera';
 
 export default function HomeScreen({ navigation }) {
+    const [image, setImage] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+
     useLayoutEffect(() => {
         navigation.setOptions({
             headerBackTitle: 'Logout'
         });
+        (async () => {
+            if (Platform.OS !== 'web') {
+                const { photosPermission } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+                const { cameraPermission } = await Camera.requestPermissionsAsync();
+            }
+        })();
     }, [navigation]);
 
-    const openPhotos = () => {
-        switch (Platform.OS) {
-            case 'ios':
-                Linking.openURL('photos-redirect://');
-                break;
-            case 'android':
-                Linking.openURL('content://media/internal/images/media');
-                break;
-            default:
-                alert('Could not open gallery app');
+    const openPhotos = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+
+        if (!result.cancelled) {
+            setImage(result.uri);
         }
     };
 
     return (
-        <Button onPress={openPhotos} raised title='Select Photo' />
+        <View style={styles.container}>
+            <Image source={image ? { uri: image } : require('../assets/placehold-200.png')} style={{ width: 200, height: 200, marginBottom: 10 }} />
+            <Button onPress={openPhotos} raised title='Select Photo' />
+            {/* <Camera type={type} sx={{ height: 50 }} >
+                <View sx={{ height: 20 }}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            setType(
+                                type === Camera.Constants.Type.back
+                                    ? Camera.Constants.Type.front
+                                    : Camera.Constants.Type.back
+                            );
+                        }}>
+                        <Text>Flip</Text>
+                    </TouchableOpacity>
+                </View>
+            </Camera> */}
+        </View>
     );
 }
 
@@ -35,4 +63,4 @@ const styles = StyleSheet.create({
         padding: 10,
         backgroundColor: 'white'
     }
-});
+});;
--- screens/Login.js
+++ screens/Login.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native';
 import { Button, Input, Image } from 'react-native-elements';
 import { StatusBar } from 'expo-status-bar';
+import Logo from '../lib/icons/Logo';
 
 export default function LoginScreen({ navigation }) {
     const [email, setEmail] = useState('');
@@ -24,17 +25,17 @@ export default function LoginScreen({ navigation }) {
     return (
         <KeyboardAvoidingView behavior='padding' style={styles.container}>
             <StatusBar style="light" />
-            <Image
+            {/* <Image
                 source={require('../assets/logo.png')}
                 style={{ width: 125, height: 125 }}
-            />
+            /> */}
+            <Logo />
             <View style={styles.inputContainer}>
                 <Input placeholder='Email' autoFocus type='email' value={email} onChangeText={setEmail} />
                 <Input placeholder='Password' secureTextEntry type='password' value={password} onChangeText={setPassword} />
                 <Button containerStyle={styles.button} onPress={signIn} title='Login' />
                 <Button containerStyle={styles.button} onPress={() => navigation.navigate('Register')} title='Register' type='outline' />
             </View>
-
 
             {/* Spacer */}
             <View style={{ height: 100 }} />
