c861bb596d52cacd1d57d3ad7fb37184d6731761
Hunter Hampton <hmh84@drexel.edu>
2022-02-02 21:20:56 -0500

    Squashed commit of the following:
    commit d5e2b112c50a0a0ecc77715c1f8f270f32d220d8
    Author: Hunter Hampton <hmh84@drexel.edu>
    Date:   Wed Feb 2 21:20:33 2022 -0500
    
        Merged settings into profile

--- screens/profile/index.tsx
+++ screens/profile/index.tsx
@@ -2,19 +2,38 @@ import React from 'react';
 import { NavigationProp } from '@react-navigation/core';
 import { ParamListBase } from '@react-navigation/routers';
 import ScreenContainer from '../../lib/components/ScreenContainer';
-import { Button } from 'react-native-paper';
-import { GlobalStackNavOptions } from '../../providers/Theme';
+import { Button, Divider, Text } from 'react-native-paper';
+import { Theme, GlobalStackNavOptions } from '../../providers/Theme';
 import { createStackNavigator } from '@react-navigation/stack';
-import SettingsScreenStack from '../settings';
+import { View, StyleSheet } from 'react-native';
+import { logOut } from '../../data/auth';
+import ChangePasswordScreen from './ChangePassword';
+import ChangeUsernameScreen from './ChangeUsername';
+import DeleteAccountScreen from './DeleteAccount';
+import ExportDataScreen from './ExportData';
 
 interface ProfileScreenProps {
 	navigation: NavigationProp<ParamListBase>;
 }
 
-const ProfileScreen = ({ navigation }: ProfileScreenProps) => {
+const ProfileIndex = ({ navigation }: ProfileScreenProps) => {
 	return (
-		<ScreenContainer scrolls>
-			<Button onPress={() => navigation.navigate('SettingsStack')}>Settings</Button>
+		<ScreenContainer style={{ justifyContent: 'space-between', alignItems: 'flex-start' }}>
+			<View style={styles.section}>
+				<Text>General</Text>
+				<Divider style={styles.divider} />
+				<Button onPress={() => navigation.navigate('ChangeUsername')}>Username</Button>
+				<Button onPress={() => navigation.navigate('ChangePassword')}>Change Password</Button>
+				<Button>FAQ</Button>
+				<Text>Data</Text>
+				<Divider style={styles.divider} />
+				<Button onPress={() => navigation.navigate('ExportData')}>Export Data</Button>
+				<Text>Account</Text>
+				<Button onPress={logOut}>Log Out</Button>
+				<Button onPress={() => navigation.navigate('DeleteAccount')} color={Theme.colors.error}>
+					Delete Account
+				</Button>
+			</View>
 		</ScreenContainer>
 	);
 };
@@ -23,9 +42,26 @@ const Stack = createStackNavigator();
 
 export default function ProfileScreenStack() {
 	return (
-		<Stack.Navigator screenOptions={{ ...GlobalStackNavOptions, presentation: 'modal' }} initialRouteName='ProfileIndex'>
-			<Stack.Screen name='ProfileIndex' component={ProfileScreen} />
-			<Stack.Screen name='SettingsStack' component={SettingsScreenStack} options={{ title: 'Settings' }} />
+		<Stack.Navigator
+			screenOptions={{ ...GlobalStackNavOptions, presentation: 'modal', headerShown: true, headerLeft: null }}
+			initialRouteName='ProfileIndex'
+		>
+			<Stack.Screen name='ProfileIndex' component={ProfileIndex} options={{ headerShown: false }} />
+			<Stack.Screen name='ChangeUsername' options={{ title: 'Change Username' }} component={ChangeUsernameScreen} />
+			<Stack.Screen name='ChangePassword' options={{ title: 'Change Password' }} component={ChangePasswordScreen} />
+			<Stack.Screen name='ExportData' options={{ title: 'Export Data' }} component={ExportDataScreen} />
+			<Stack.Screen name='DeleteAccount' options={{ title: 'Delete Account' }} component={DeleteAccountScreen} />
 		</Stack.Navigator>
 	);
 }
+
+const styles = StyleSheet.create({
+	section: {
+		display: 'flex',
+		alignItems: 'flex-start',
+	},
+	divider: {
+		width: '100%',
+		height: 3,
+	},
+});
