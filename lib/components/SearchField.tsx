91e832b5372d1fb83ab63f7556d2f8aea5c68e48
Hunter Hampton <hmh84@drexel.edu>
2022-02-11 14:36:28 -0500

    Fixxed searchField style

--- lib/components/SearchField.tsx
+++ lib/components/SearchField.tsx
@@ -13,6 +13,7 @@ export default function SearchField({ onQuery, style, ...rest }: SearchFieldProp
 	const styles = StyleSheet.create({
 		input: {
 			height: 35,
+			backgroundColor: 'white',
 			borderRadius: Theme.borderRadius,
 			borderTopLeftRadius: Theme.borderRadius,
 			borderTopRightRadius: Theme.borderRadius,
